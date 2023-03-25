abde8300843fd96fbf33647d05374140de8b83dd
Saljooq <altaf.saljooq@outlook.com>
2023-03-25 14:17:49 -0500

    Update markdown.tsx

--- src/routes/markdown.tsx
+++ src/routes/markdown.tsx
@@ -53,8 +53,6 @@ const MarkdownPage: Component = () => {
 					<option value={page.title}>{page.title}</option>
 					}
 				</For>
-				<option>test 1</option>
-				<option>test 2</option>
 			</select>
 			</label>
 
