6b6fbc51e40128b8123c287d3fd0893c8f7210ce
Saljooq <altaf.saljooq@outlook.com>
2023-03-25 01:45:15 -0500

    Update markdown.tsx

--- src/routes/markdown.tsx
+++ src/routes/markdown.tsx
@@ -48,7 +48,7 @@ const MarkdownPage: Component = () => {
             onChange={(e)=> setPage(getSelectedPage(e.target.value))}
           >
             <option selected disabled>Choose a markdown page</option>
-            <For each={pages()}>{page => <option selected
+            <For each={pages()}>{page => <option value={page.title}
               >{page.title}</option>}
             </For>
           </select>
