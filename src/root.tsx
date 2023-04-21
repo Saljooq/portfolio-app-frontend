4d7d49a3684d939b8b5de266341e9da33bcc2f91
Saljooq <altaf.saljooq@outlook.com>
2023-04-21 14:38:29 -0500

    Very nice commit - upgrades tabs and gives it very modern and responsive feel

--- src/root.tsx
+++ src/root.tsx
@@ -20,12 +20,16 @@ import index2 from "./routes/index2"
 
 export default function Root() {
   const location = useLocation();
-  const active = (path: string) =>
-    path == location.pathname
-      ? "border-sky-600"
-      : "border-transparent hover:border-stone-400";
+
 
   const [count, setCount] = createSignal(0);
+
+  const active = (path: string) =>
+    path == location.pathname
+      ? "border-b-4 border-sky-500"
+      : "border-b-4 border-transparent";
+
+  const aClass = "inline-flex p-3 border-b-2 border-transparent rounded-t-lg hover:text-sky-500 hover:border-sky-300  group";
 
   console.log("created a count: "+ count())
   return (
@@ -39,25 +43,22 @@ export default function Root() {
         <div class="h-screen overflow-y-auto">
         <Suspense>
           <ErrorBoundary>
-            <nav class="bg-sky-800">
-              <ul class="container flex items-center p-3 text-gray-200">
-                <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-                    <A href="/">Index - {count()}</A>
+            <nav class="border-b border-sky-800 bg-inherit dark:border-gray-700">
+              <ul class="container flex flex-wrap -mb-px text-sm font-medium text-center  dark:text-black">
+                <li class={`mr-2 ${active('/')} mx-1.5 sm:mx-6`}>
+                    <A href="/" class={aClass}>Index - {count()}</A>
                 </li>
-                <li class={`border-b-2 ${active("/index2")} mx-1.5 sm:mx-6`}>
-                    <A href="/index2">Index2 - {count()}</A>
+                <li class={`mr-2 ${active('/about')} mx-1.5 sm:mx-6`}>
+                  <A href="/about" class={aClass}>About</A>
                 </li>
-                <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-                  <A href="/about">About</A>
+                <li class={`mr-2 ${active('/markdown')} mx-1.5 sm:mx-6`}>
+                  <A href="/markdown" class={aClass}>Markdown</A>
                 </li>
-                <li class={`border-b-2 ${active("/markdown")} mx-1.5 sm:mx-6`}>
-                  <A href="/markdown">Markdown</A>
+                <li class={`mr-2 ${active('/mdmodalpage')} mx-1.5 sm:mx-6`}>
+                  <A href="/mdmodalpage" class={aClass}>MD Modal</A>
                 </li>
-                <li class={`border-b-2 ${active("/mdmodalpage")} mx-1.5 sm:mx-6`}>
-                  <A href="/mdmodalpage">MD Modal</A>
-                </li>
-                <li class={`border-b-2 ${active("/editor")} mx-1.5 sm:mx-6`}>
-                  <A href="/editor">Editor</A>
+                <li class={`mr-2 ${active('/editor')} mx-1.5 sm:mx-6`}>
+                  <A href="/editor" class={aClass}>Editor</A>
                 </li>
               </ul>
             </nav>
